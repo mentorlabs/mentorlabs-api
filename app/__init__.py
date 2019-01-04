be3ba62632930fc2302c551164c5860e5b881bbf
Logan Wright <logan.wright@valic.com>
2019-01-03 21:59:00 -0600

    initial models and controllers

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,15 @@
-# init
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_marsmhallow import Marshmallow
+
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+migrate = Migrate(app, db)
+
+if __name__ == '__main__':
+    app.run()
