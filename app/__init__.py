a8aa09db036cc5438174d9168588ee092783c500
LBWright <lwright.js@gmail.com>
2019-01-06 12:39:22 -0600

    setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_marsmhallow import Marshmallow
+from flask_marshmallow import Marshmallow
 
 from config import Config
 
