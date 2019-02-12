cc214e54c464fe3bad0b241f11f27a8b8d871be5
LBWright <lwright.js@gmail.com>
2019-02-11 21:47:55 -0600

    removed python to restart in Node

--- app/api/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('api', __name__)
-
-import app.api.users
-import app.api.auth
--- app/models/badge.py
+++
@@ -1,8 +0,0 @@
-from app import db
-
-
-class Badge(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128), index=True, unique=True)
-    description = db.Column(db.String(128), index=True)
-    # user relationship
