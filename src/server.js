ef18592761a6fd2ad4403f24ea0f6456848beee7
Ely Alamillo <ely0013@gmail.com>
2019-02-13 21:35:39 -0600

    add json parser and urlencoding parser

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
+import bodyParser from 'body-parser'
 import config from './config'
 import { connect } from './utils/db'
 import { router } from './resources/router'
@@ -8,18 +9,20 @@ import { router } from './resources/router'
 export const app = express()
 
 app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(morgan('dev'))
 router(app)
 
 export const start = async () => {
-    try {
-        await connect()
-        app.listen(config.port, () => {
-            console.log('------------------------------------')
-            console.log(`API Up and Running on PORT: ${config.port}` )
-            console.log('------------------------------------')
-        })
-    } catch (e) {
-        console.error(e)
-    }
+  try {
+    await connect()
+    app.listen(config.port, () => {
+      console.log('------------------------------------')
+      console.log(`API Up and Running on PORT: ${config.port}`)
+      console.log('------------------------------------')
+    })
+  } catch (e) {
+    console.error(e)
+  }
 }
