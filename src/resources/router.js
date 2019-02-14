fb2d1e6fd3e35873d1fe3e383f7ac5edbee0ffdc
Ely Alamillo <ely0013@gmail.com>
2019-02-13 21:37:23 -0600

    wire up ideas routes

--- src/resources/router.js
+++ src/resources/router.js
@@ -1,5 +1,6 @@
 import userRouter from './user/user.router'
+import ideaRouter from './idea/idea.router'
 
-export const router = app => (
-    app.use('/api/users', userRouter)
-)
+export const router = app => {
+  app.use('/api/users', userRouter), app.use('/api/ideas', ideaRouter)
+}
