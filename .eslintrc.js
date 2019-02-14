6bbcb57f95d4a566202fd5ae70dd80383279cde1
Ely Alamillo <ely0013@gmail.com>
2019-02-14 00:03:38 -0600

    removed plugin thta wasn't workign added replacement

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,21 @@
 module.exports = {
-    parserOptions: {
-      sourceType: 'module'
-    },
-    parser: 'babel-eslint',
-    env: {
-      node: true
-    },
-    extends: [
-      'standard',
-      'prettier',
-      'prettier/standard',
-      'plugin:jest/recommended'
-    ],
-    plugins: ['prettier', 'jest'],
-    rules: {
-      'promise/catch-or-return': 'error',
-      'prettier/prettier': [
-        'error',
-        {
-          'singleQuote': true,
-          'semi': false
-        }
-      ]
-    }
+  parserOptions: {
+    sourceType: 'module'
+  },
+  parser: 'babel-eslint',
+  env: {
+    node: true
+  },
+  extends: ['standard', 'prettier', 'plugin:jest/recommended'],
+  plugins: ['prettier', 'jest', 'prettier/recommended'],
+  rules: {
+    'promise/catch-or-return': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: false
+      }
+    ]
   }
+}
