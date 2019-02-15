237e17134031738f24565ffc3348eed239e0d7d4
Ely Alamillo <ely0013@gmail.com>
2019-02-14 20:59:33 -0600

    wired up new routes

--- src/resources/idea/idea.router.js
+++ src/resources/idea/idea.router.js
@@ -3,6 +3,9 @@ import { ideaControllers } from './idea.controllers'
 
 const ideaRouter = Router()
 
-ideaRouter.post('/create', ideaControllers.createOne)
+ideaRouter.get('/', ideaControllers.getAll)
+ideaRouter.post('/', ideaControllers.createOne)
+ideaRouter.put('/update/:id', ideaControllers.updateOne)
+ideaRouter.put('/destoy', ideaControllers.destroy)
 
 export default ideaRouter
